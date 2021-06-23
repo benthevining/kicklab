4f8fbce1c6a9a59c3bb6ab77d190d0b06f6c8343
Ben Vining <ben.the.vining@gmail.com>
2021-06-23 16:52:04 -0700

    Update kicklab_gui.h

--- Source/modules/kicklab_gui/kicklab_gui.h
+++ Source/modules/kicklab_gui/kicklab_gui.h
@@ -9,13 +9,13 @@ vendor:            Ben Vining
 version:           0.0.1
 name:              kicklab_gui
 description:       GUI for Kicklab
-dependencies:      bv_gui kicklab_state
+dependencies:      bv_plugin_gui kicklab_state
 
 END_JUCE_MODULE_DECLARATION
 
 #endif
 
-#include <bv_gui/bv_gui.h>
+#include <bv_plugin_gui/bv_plugin_gui.h>
 #include <kicklab_state/kicklab_state.h>
 
 namespace Kicklab
